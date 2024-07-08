3de146adba528dd0fce80dcc3df2b1fe3077cccc
Dmitriy <94927331+DMITRIYBY@users.noreply.github.com>
2024-07-08 14:30:28 +0300

    fix: getUrl method fix

--- modsen-task/src/components/PlaceInfo/PlaceInfo.tsx
+++ modsen-task/src/components/PlaceInfo/PlaceInfo.tsx
@@ -7,7 +7,14 @@ interface PlaceDetailsProps {
 export const PlaceInfo: React.FC<PlaceDetailsProps> = ({ place }) => {
     if (!place) return null;
 
-    const { name, formatted_address, rating, user_ratings_total, photos } = place;
+    const { name, formatted_address, rating, user_ratings_total } = place;
+
+    const getPlacePhoto = () => {
+        if (place.photos && place.photos.length > 0) {
+            const photoUrl = place.photos[0].getUrl({ maxWidth: 200, maxHeight: 200 });
+            return <img src={photoUrl} alt={place.name || 'Place'} />;
+        }
+    };
 
     return (
         <div>
@@ -15,12 +22,7 @@ export const PlaceInfo: React.FC<PlaceDetailsProps> = ({ place }) => {
             <p>{formatted_address}</p>
             <p>{rating && `Rating: ${rating}`}</p>
             <p>{user_ratings_total && `Total Ratings: ${user_ratings_total}`}</p>
-            {photos && photos.length > 0 && (
-                <img
-                    src={photos[0].getURI({ maxWidth: 200, maxHeight: 200 })}
-                    alt={name}
-                />
-            )}
+            {getPlacePhoto()}
         </div>
     );
 };
